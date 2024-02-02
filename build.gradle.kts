4e177f0e56b5f73c008ba6ddff85017781d51535
Joel <acostajoel0001@gmail.com>
2024-02-02 13:08:02 -0500

    Curso finalizado spring security basico

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "dev.aceelv.login"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
